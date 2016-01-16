d9e31b81c9dd0302e1b90be0133013dc1fec18f1
David Arvelo <david@davidarvelo.com>
2016-01-15 23:58:04 -0500

    Begin Phase 2: Navigation
    Stub out some functions and document them.

--- src/classes/root-app.js
+++ src/classes/root-app.js
@@ -84,10 +84,10 @@ class RootApp extends App {
     }
 
     /**
-    * Parses a query string.
-    * @param {string} queryString A string analogous to window.location.search.
-    * @return {object} An object containing the query parameters as the object's keys with values.
-    */
+     * Parses a query string.
+     * @param {string} queryString A string analogous to window.location.search.
+     * @return {object} An object containing the query parameters as the object's keys with values.
+     */
     parseQueryString(queryString) {
         if (queryString[0] === '?') {
             queryString = queryString.slice(1);
@@ -104,6 +104,97 @@ class RootApp extends App {
             return memo;
         }, {});
         return queryParams;
+    }
+
+    /**
+     * @typedef NavigationRequest
+     * @type {object}
+     * @property {string} address The address of the destination `Route` to navigate to.
+     * @property {object} params The params to apply during navigation while traversing each mount in the Ether app's mount tree, as if these params were parsed from the URL.
+     * @property {object|undefined} queryParams Any queryParams to apply during each step in the mount traversal.
+     */
+
+    /**
+     * Navigates to a new URL path on the Ether application using either a URL string or a special `NavigationRequest`
+     * Is called on popstate if configured to, page landing if configured to, intercepted links if configured to, or manually.
+     * @param {string|NavigationRequest} destination The destination `Route` to navigate to. Can be a URL string with or without a querystring, or a `NavigationRequest`.
+     */
+    navigate(destination) {
+        // make sure to compound and forward params in any case below
+        // push params onto the stack (now just a recent-params map)
+        // if we've got * routes, do those first
+        // if urlpath => app
+        //     call app to make it use its mountmapper and route method
+        // if urlpath => route
+        //     call render with params or show if params equal
+        //     pushState() if this isn't page load
+        //
+        // @TODO: write a test that makes sure passed params match expectedParams() during navigation
+        // @TODO: parse queryParams
+        //
+        // if no param diffs do nothing: link was clicked twice
+        //
+        // if during a push, the paths and params are exactly the same as on all stacks, do nothing. we're navigating to a page we're already on.
+        // we could actually just store the current path on the root app after a successful route and save ourselves the recursive search by checking against it.
+        // we can't store the path on the root until the routing succeeds because we have to be careful the routing won't end in a 404.
+        //
+        // find mount point of divergence
+
+
+        // fullURL can be from popstate event, window.load event, link interception, or manual
+        //
+        // if (this.fullUrl is the same or URL pathname is same and query params are same but in a diff order (qP diff returns null)) {
+        //     // same link was clicked twice
+        //     return;
+        // }
+        // let routingTrace = this._buildPath();
+        // if (routingTrace.is404) {
+        //     // notify user of 404 and let them handle it their way
+        // } else {
+        //     this._constructState(routingTrace).catch(err => {;
+        //         // notify user how this happened
+        //     });
+        // }
+    }
+
+    /**
+     * @typedef NavigationStep
+     * @type {object}
+     * @property {string} breadcrumb The string representing a mount that's mounted on a particular app.
+     * @property {object} params The params required by the mount.
+     */
+
+    /**
+     * @typedef RoutingTrace
+     * @type {object}
+     * @property {string} result `success` or `404`.
+     * @property {object|undefined} diverge An object that describes where the path in the URL diverged when tracing the new navigation destination from the URL as compared to the previous URL. `undefined` if the URL path was the same and only params or query params changed.
+     * @property {App} diverge.app The app where the path diverged.
+     * @property {object} diverge.params The new params from the URL at the point where the path diverged.
+     * @property {Array.<NavigationStep>} steps The mounts, in order from the root to the leaf route, that we can follow to the navigation route destination.
+     */
+
+    /**
+     * Builds an object representing a trace through the app hierarchy down to the target route found by parsing the URL (querystring included).
+     * @private
+     * @param {string} url The URL to parse with querystring, if any, included.
+     * @return {RoutingTrace} A routing trace object that can be used to construct app state: successful navigation or 404.
+     */
+    _buildPath(url) {
+        // test this fn by stubbing out _constructState()
+    }
+
+    /**
+     * Constructs app state by using the routing trace object to prerender, render, and deactivate routes as needed.
+     * @private
+     * @param {RoutingTrace} routingTrace The routing trace object used to construct the app state.
+     * @return {Promise} A promise that, if rejected, means a user-defined prerender/render/deactivate promise rejected and the Ether app is now in an undefined state.
+     */
+    _constructState(routingTrace) {
+        // perform recursive prerender/deactivate/render for mounts and cMounts.
+        // has handles to all apps/routes in path, uses them to send the approp. signals from leaf to divergence point w/ diff in params for that node & expectedParams.
+        // sends queryParams diff to every route on the navigation path.
+        // promise-based stuff happens here.
     }
 }
 
--- src/classes/route.js
+++ src/classes/route.js
@@ -106,10 +106,34 @@ class Route extends Modifiable {
         return;
     }
 
+    /**
+     * User-defined function meant to perform any data-gathering or rendering when this route is navigated to, *before* the previously-navigated-to route is deactivated.
+     * @param {object|null} params All parameters parsed from the URL that are only also listed in this route's `expectedParams()` method. `null` if either no params are expected by this route or if no mount up to this point had any params to be parsed.
+     * @param {object|null} queryParams All querystring parameters parsed from the URL or `null` if there weren't any.
+     * @param {object|null} diffs By property, the calculated difference between the params/queryParams passed when the route was last rendered vs. those being passed from the current URL. `null` if there were no differences, signifying that the route was deactivated and is now being navigated to via the exact same URL as before (common when the user clicks the Back button, for example).
+     * @param {object} diffs.params An object where each property is an `Array` with two values: the value of the param when this route was last rendered, and the value of the param parsed from the current URL. If there is no difference between the two, the property for that param won't be present here, as this object only shows differences between past and present params.
+     * @param {object} diffs.queryParams The same as `diffs.params` but for querystring parameters parsed from the URL. If a query param was added since last render, the previous value (array index 0) will be `undefined`. If a query param was removed since last render, the new value (array index 1) will be `undefined`.
+     * @return {Promise} A promise that, when resolved, means all prerender actions (e.g. AJAX data retrieval/storage, populating/showing views, etc.) have finished and further navigation operations can continue. If the promise is rejected, the Ether app will be in an undefined state.
+     */
+    prerender(params, queryParams, diffs) { }
+
+    /**
+     * User-defined function meant to perform any data-gathering or rendering when this route is navigated to, *after* the previously-navigated-to route is deactivated.
+     * @param {object|null} params All parameters parsed from the URL that are only also listed in this route's `expectedParams()` method. `null` if either no params are expected by this route or if no mount up to this point had any params to be parsed.
+     * @param {object|null} queryParams All querystring parameters parsed from the URL or `null` if there weren't any.
+     * @param {object|null} diffs By property, the calculated difference between the params/queryParams passed when the route was last rendered vs. those being passed from the current URL. `null` if there were no differences, signifying that the route was deactivated and is now being navigated to via the exact same URL as before (common when the user clicks the Back button, for example).
+     * @param {object} diffs.params An object where each property is an `Array` with two values: the value of the param when this route was last rendered, and the value of the param parsed from the current URL. If there is no difference between the two, the property for that param won't be present here, as this object only shows differences between past and present params.
+     * @param {object} diffs.queryParams The same as `diffs.params` but for querystring parameters parsed from the URL. If a query param was added since last render, the previous value (array index 0) will be `undefined`. If a query param was removed since last render, the new value (array index 1) will be `undefined`.
+     * @return {Promise} A promise that, when resolved, means all render actions (e.g. AJAX data retrieval/storage, populating/showing views, etc.) have finished and navigation can continue on to completion. If the promise is rejected, the Ether app will be in an undefined state.
+     */
     render() { }
-    destroy() { }
-    willHide() { }
-    willShow() { }
+
+    /**
+     * A user-defined function that's called when the URL has changed and another, different route is to be rendered instead. This is a good opportunity to use CSS to hide all outlets or views in use by this route, as outlets are exclusive to a single route.
+     * @return {Promise} A promise signifying when any actions relevant to deactivation (cleanup, hiding views or UI components, etc.) have all finished.
+     * @return {Promise} A promise that, when resolved, means all deactivate actions (e.g. sending data to the server, data cleanup for GC, hiding outlets, views, or UI components, etc.) have finished and navigation can continue toward rendering the new route destination. If the promise is rejected, the Ether app will be in an undefined state.
+     */
+    deactivate() { }
 }
 
 export default Route;
