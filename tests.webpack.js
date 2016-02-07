b76e7da09186cac64a8fadc0c16020eb7bda3c91
David Arvelo <david@davidarvelo.com>
2016-02-06 23:06:33 -0500

    Add acceptance tests and utils tests to Karma

--- tests.webpack.js
+++ tests.webpack.js
@@ -2,10 +2,16 @@
 var componentsContext = require.context('./src', true, /\.js?$/);
 componentsContext.keys().forEach(componentsContext);
 
+// require acceptance tests
+var acceptanceTestsContext = require.context('./test/acceptance', true, /\.spec\.js?$/);
+acceptanceTestsContext.keys().forEach(acceptanceTestsContext);
+
 // require functional tests
 var functionalTestsContext = require.context('./test/functional', true, /\.spec\.js?$/);
 functionalTestsContext.keys().forEach(functionalTestsContext);
 
 // require unit tests
-var unitTestsContext = require.context('./test/unit/classes', true, /\.spec\.js?$/);
-unitTestsContext.keys().forEach(unitTestsContext);
+var classesUnitTestsContext = require.context('./test/unit/classes', true, /\.spec\.js?$/);
+classesUnitTestsContext.keys().forEach(classesUnitTestsContext);
+var utilUnitTestsContext = require.context('./test/unit/utils', true, /\.spec\.js?$/);
+utilUnitTestsContext.keys().forEach(utilUnitTestsContext);
