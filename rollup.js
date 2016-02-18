387fe94143b9deadc21cd97c51f5953137e602e9
David Arvelo <david@davidarvelo.com>
2016-02-17 22:38:22 -0500

    Apply banner after uglify plugin

--- rollup.js
+++ rollup.js
@@ -4,35 +4,41 @@ var uglify = require('rollup-plugin-uglify');
 
 var dest = 'dist/';
 var version = require('./package.json').version;
+var banner = '/* Ether version ' + version + ' */';
+
+function addBanner(code) {
+    return banner + '\n' + code;
+}
 
 rollup.rollup({
     entry: 'src/index.js',
+    banner: banner,
     plugins: [
         babel(),
         uglify(),
+        // banner property in bundle.write
+        // doesn't seem to be working after
+        // the uglify plugin has run
+        {transformBundle: addBanner}
     ],
 }).then(function (bundle) {
-    var banner = '/* Ether version ' + version + ' */';
     return Promise.all([
         bundle.write({
             format: 'amd',
             moduleId: 'ether',
             exports: 'named',
             dest: dest + 'ether.amd.js',
-            banner: banner,
         }),
         bundle.write({
             format: 'cjs',
             exports: 'named',
             dest: dest + 'ether.cjs.js',
-            banner: banner,
         }),
         bundle.write({
             format: 'iife',
             moduleName: 'Ether',
             exports: 'named',
             dest: dest + 'ether.global.js',
-            banner: banner,
             sourcemap: 'inline',
         }),
     ]);
